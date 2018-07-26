4c508b2f654cc0bd47268e35f48a98f7ee07f614
Kevin Jacobs <frevin8r@gmail.com>
2018-07-26 16:03:52 -0700

    Init server connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
 require('dotenv/config');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(`Connected to PORT: ${PORT}`));
