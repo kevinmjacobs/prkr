0bf28354a07c1378dc7536f4b9a163bd04736b06
Kevin Jacobs <frevin8r@gmail.com>
2018-07-26 21:42:23 -0700

    Minor style cleanup

--- src/components/nav-bar/NavBar.style.js
+++ src/components/nav-bar/NavBar.style.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  background-color: #a86ed8;
-  color: #ffffff;
+  border-bottom: .5px solid #a86ed8
+  color: #a86ed8;
+  height: 40px;
+  line-height: 40px;
+  text-align: right;
 `
