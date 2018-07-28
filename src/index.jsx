09bc682f2c56440820e55b90baf6087bef6bf638
Kevin Jacobs <frevin8r@gmail.com>
2018-07-28 15:36:45 -0700

    Set up store & reducers Create reducers for items and logo

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App.jsx';
 
+import { combineReducers, createStore } from 'redux';
+
+const itemReducer = (state = [], action) => {
+  return state;
+}
+
+const logoReducer = (state = '', action) => {
+  return state;
+}
+
+const allReducers = combineReducers({
+  items: itemReducer,
+  logo: logoReducer
+});
+
+const store = createStore(allReducers, {
+  items: ['Help', 'Services', 'About'],
+  logo: "https://s3-us-west-1.amazonaws.com/prkr-img/temp-logo.png"
+});
+
+console.log(store.getState());
+
 render(
   <App />,
   document.getElementById("app")
