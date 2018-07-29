34409c8f3580dbbd427cef0511dc2b71774b7ccf
Kevin Jacobs <frevin8r@gmail.com>
2018-07-28 20:17:49 -0700

    Add ipsum lorem Title and description

--- src/components/home-summary/HomeSummary.jsx
+++ src/components/home-summary/HomeSummary.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import loremIpsum from 'lorem-ipsum';
+
 import { HomeSummaryH1, HomeSummaryH3 } from './HomeSummary.style.js';
 
 const HomeSummary = () => {
+  let summary = loremIpsum({
+    count: 3,
+    units: 'words'
+  })
+  let description = loremIpsum({
+    count: 5,
+    units: 'words',
+  })
   return(
     <div>
-      <HomeSummaryH1>Title</HomeSummaryH1>
-      <HomeSummaryH3>Summary</HomeSummaryH3>
+      <HomeSummaryH1>{summary}</HomeSummaryH1>
+      <HomeSummaryH3>{description}</HomeSummaryH3>
     </div>
   )
 }
