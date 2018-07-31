60285d2bf7301ae91f33b5e9253d9485f980e90b
Kevin Jacobs <frevin8r@gmail.com>
2018-07-30 19:53:22 -0700

    Create Booking component Add to Home component, create style file

---
+++ src/components/home/booking/Booking.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Booking extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <div>
+        Booking
+      </div>
+    )
+  }
+}
+
+export default Booking;
