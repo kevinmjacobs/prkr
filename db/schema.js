def25e8d625e74349f808c4ef038a599bdd9515a
Kevin Jacobs <frevin8r@gmail.com>
2018-08-07 20:51:34 -0700

    Update char limits, seed data Populate valet table with 500 dummy records

--- db/schema.js
+++ db/schema.js
@@ -2,7 +2,18 @@ const { client } = require('./database');
 
 client.connect();
 
-client.query(`CREATE TABLE valets(id SERIAL PRIMARY KEY, first_name VARCHAR(100), last_name VARCHAR(100), address VARCHAR(255), city VARCHAR(255), state VARCHAR(10), zip VARCHAR(10), phone VARCHAR(10), email VARCHAR(255));`, (err, data) => {
+client.query(`
+  CREATE TABLE valet(
+    id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(100) NOT NULL, 
+    last_name VARCHAR(100) NOT NULL, 
+    address VARCHAR(255) NOT NULL, 
+    city VARCHAR(255) NOT NULL, 
+    state VARCHAR(10) NOT NULL, 
+    zip VARCHAR(10) NOT NULL, 
+    phone VARCHAR(20) NOT NULL, 
+    email VARCHAR(255) NOT NULL);
+  `, (err, data) => {
   (err) && console.log(err.stack);
   client.end();
 });
