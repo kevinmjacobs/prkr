93500e3f870daa2e44a6c740fabd72fbaef5f785
Kevin Jacobs <frevin8r@gmail.com>
2018-09-04 09:44:37 -0700

    Add bcrypt to signup POST Pass hashed password to db

--- server/signup/controller.js
+++ server/signup/controller.js
@@ -1,16 +1,23 @@
 const SignupModel = require('./model.js');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   post: (req, res) => {
     const email = req.body.email;
-    const password = req.body.password;
-    SignupModel.post(email, password, (err, data) => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
       if (err) {
-        console.log(err.stack);
-        res.send('User not created').status(400);
+        console.log(err.stack)
+        res.send('Error hashing password').status(400);
       } else {
-        res.send(data).status(201);
+        SignupModel.post(email, hash, (err, data) => {
+          if (err) {
+            console.log(err.stack);
+            res.send('User not created').status(400);
+          } else {
+            res.send(data).status(201);
+          }
+        });
       }
-    })
+    });
   }
 }
