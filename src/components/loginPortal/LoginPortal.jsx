a6831c3ded730aae7303a6360ad417c81a4bf988
Kevin Jacobs <frevin8r@gmail.com>
2018-09-03 20:16:01 -0700

    Create event handlers for Submit inputChange and submitSignupForm

--- src/components/loginPortal/LoginPortal.jsx
+++ src/components/loginPortal/LoginPortal.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Login } from "./login/Login.jsx";
-import { Signup } from "./signup/Signup.jsx"
+import Signup from "./signup/Signup.jsx"
 import { MainDiv, LoginTab, SignupTab } from './LoginPortal.style.js';
 
 class LoginPortal extends React.Component {
--- src/components/loginPortal/signup/Signup.jsx
+++ src/components/loginPortal/signup/Signup.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
+import { findDOMNode } from 'react-dom';
 import { SignupForm, SignupEmail, SignupPassword, SubmitSignup } from './Signup.style.js';
 
-export const Signup = () => (
-  <SignupForm>
-    <SignupEmail type="text" placeholder="Email"/>
-    <SignupPassword type="text" placeholder="Password"/>
-    <SignupPassword type="text" placeholder="Re-Enter Password"/>
-    <SubmitSignup type="submit" value="Sign Up" />
-  </SignupForm>
-);
+export default class Signup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      rePassword: ''
+    }
+    this.submitSignupForm = this.submitSignupForm.bind(this);
+  }
+
+  inputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitSignupForm(e) {
+    if (this.state.password !== this.state.rePassword) {
+      this.setState({
+        password: '',
+        rePassword: ''
+      }, () => {
+        findDOMNode(this.refs.password).value = "";
+        findDOMNode(this.refs.rePassword).value = "";
+        window.alert("Passwords do not match - try again");
+      })
+    }
+  }
+
+  render() {
+    return(
+      <SignupForm onSubmit={e => { e.preventDefault(); }}>
+        <SignupEmail type="text" name='email' placeholder="Email" onKeyUp={ (e) => { this.inputChange(e) } }/>
+        <SignupPassword type="password" name='password' ref="password" placeholder="Password" onKeyUp={ (e) => { this.inputChange(e) } }/>
+        <SignupPassword type="password" name='rePassword' ref="rePassword" placeholder="Re-Enter Password" onKeyUp={ (e) => { this.inputChange(e) } }/>
+        <SubmitSignup type="submit" value="Sign Up" onClick={ (e) => {  this.submitSignupForm(e) } } />
+      </SignupForm>
+    )
+  }
+}
+
+
